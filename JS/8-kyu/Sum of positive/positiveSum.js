7eb775d500a6538658bf03cc2e494d65a5bc9768
Éric Férole <eferole@pixelcircus.ca>
2020-04-24 10:24:23 -0400

    Finnishing reduce

--- JS/8-kyu/Sum of positive/positiveSum.js
+++ JS/8-kyu/Sum of positive/positiveSum.js
@@ -1,9 +1,28 @@
+// https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+
 const myArray = [1,2,-5,10,-2]
 
 const positiveSum = (arr) => {
-  arr.reduce((total, value) => {
-    return total + value;
+  const myNewArray = []
+
+arr.forEach(number => {
+
+  if (number >= 0) {
+    return myNewArray.push(number);
+  };
+});
+
+ return myNewArray.reduce((total, value) => {
+     return total + value;
   });
 }
 
 console.log(positiveSum(myArray))
+
+
+// for (let positiveNum in arr) {
+//   if (positiveNum >= 0) {
+//     myNewArray.push(positiveNum)
+//   }
+//   console.log(myNewArray)
+// }
